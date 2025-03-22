4762172cb1c01516214d0054a45bf62ad9d6762f
Magnus Olofsson <magnusolofsson@outlook.com>
2025-03-22 09:02:07 +0100

    Solved the two first challenges, all three parts.

---
+++ AbsurdArithmetic/Program.cs
@@ -0,0 +1,97 @@
+﻿const string fileName="input.txt";
+using StreamReader fileInput = System.IO.File.OpenText(fileName);
+
+int value1 = int.Parse(fileInput.ReadLine().Split(" ").Last());
+int value2 = int.Parse(fileInput.ReadLine().Split(" ").Last());
+int value3 = int.Parse(fileInput.ReadLine().Split(" ").Last());
+_ = fileInput.ReadLine();
+List<int> roomQualities = [];
+while (fileInput.EndOfStream == false)
+{
+    string line = fileInput.ReadLine();
+    roomQualities.Add(int.Parse(line));
+}
+CurrencyConverter converter = new(value3, value2, value1);
+
+part1();
+part2();
+part3();
+
+
+void part3()
+{
+    var roomList = roomQualities.Select(r => new Room(r, converter.Convert(r))).ToList();
+    var bestRoomForBudget = roomList.Where(r => r.Price <= 15_000_000_000_000).Max();
+    Console.WriteLine($"Part 3\nMax price for room within budget is: {bestRoomForBudget.Quality}");
+    
+}
+void part2()
+{
+    int qualitySum = roomQualities[1..].Where(q => q % 2 == 0).Sum();
+    long part2Sum = converter.Convert(qualitySum);
+    Console.WriteLine($"Part 2\nSum: {part2Sum}");
+}
+void part1()
+{
+    roomQualities.Sort();
+    int medianRoomQuality = roomQualities[(roomQualities.Count - 1)/2];
+    Console.WriteLine($"Part 1. \nThe price for the median room qualities is: {converter.Convert(medianRoomQuality)}");
+}
+
+class Room : IComparable<Room>
+{
+    private int _quality;
+    private long _price;
+
+    public int Quality
+    {
+        get => _quality;
+        private set => _quality = value;
+    }
+
+    public long Price
+    {
+        get => _price;
+        set => _price = value;
+    }
+
+    public Room(int quality, long price)
+    {
+        _quality = quality;
+        _price = price;
+    }
+
+    public int CompareTo(Room? other)
+    {
+        if (this.Price < other.Price)
+        {
+            return -1;
+        }
+        else if (this.Price > other.Price)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
+class CurrencyConverter
+{
+    private int exponent;
+    private int multiplier;
+    private int addvalue;
+
+    public CurrencyConverter(int exponent, int multiplier, int addvalue)
+    {
+        this.exponent = exponent;
+        this.multiplier = multiplier;
+        this.addvalue = addvalue;
+    }
+
+    public long Convert(int roomQuality)
+    {
+        return (long)Math.Pow(roomQuality, exponent) * multiplier + addvalue;
+    }
+}
---
+++ CompassCalibration/Program.cs
@@ -0,0 +1,62 @@
+﻿const string fileName = "input.txt";
+var allLines = File.ReadAllLines(fileName);
+//Part 1 and 2
+//var numbers = allLines.Where(l=>l.StartsWith('-')==false && l.StartsWith('+')==false).Select(int.Parse).ToList();
+//Part 3
+var chars = allLines.Where(l => l.StartsWith('-') == false && l.StartsWith('+') == false);
+bool second = false;
+List<int> numbers = new List<int>();
+string thisNumber = string.Empty;
+foreach (var line in chars)
+{
+    thisNumber = $"{thisNumber}{line}";
+    if(second==true)
+    {
+        numbers.Add(int.Parse(thisNumber));
+        thisNumber = string.Empty;
+    }
+    second = !second;
+}
+var initialNumber = numbers.First();
+//Part 3
+numbers.RemoveAt(0);
+
+//Part 1
+//var commands = allLines.Where(l => l.StartsWith('-') || l.StartsWith('+')).ToList().First();
+//Part 2 and 3
+var commands = allLines.Where(l => l.StartsWith('-') || l.StartsWith('+')).ToList().First().Reverse().ToArray();
+var sum=initialNumber;
+
+// Part 1 and 2
+/*
+var index =1;
+ foreach(char command in commands)
+{
+    if (command == '+')
+    {
+        sum += numbers[index];
+    }
+    else if (command == '-')
+    {
+        sum -= numbers[index];
+    }
+
+    index++;
+}*/
+
+var index = 0;
+foreach (var number in numbers)
+{
+    if (commands[index] == '+')
+    {
+        sum += number;
+    }
+    else
+    {
+        sum -= number;
+    }
+
+    index++;
+}
+
+Console.WriteLine(sum);
