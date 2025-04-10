a2d4a54cc0856ec3921ee42da51ed6e21d031c1a
Magnus Olofsson <magnusolofsson@outlook.com>
2025-04-10 20:17:19 +0200

    Added CyclopsChaos and solved part1

---
+++ CyclopsChaos/Program.cs
@@ -0,0 +1,25 @@
+﻿const string fileName ="input.txt";
+List<int[]> rows = File.ReadAllLines(fileName).Select(line => line.Split(' ').Select(int.Parse).ToArray()).ToList();
+Part1();
+void Part1()
+{
+    int lowestRowSum = int.MaxValue;
+    int lowestColSum = int.MaxValue;
+    foreach (var row in rows)
+    {
+        var sum = row.Sum();
+        lowestRowSum = Math.Min(lowestRowSum, sum);
+    }
+
+    for (int i = 0; i < rows[0].Length; i++)
+    {
+        int thisColSum = 0;
+        foreach (var row in rows)
+        {
+            thisColSum += row[i];
+        }
+        lowestColSum = Math.Min(lowestColSum, thisColSum);
+    }
+    int lowestSum = Math.Min(lowestRowSum, lowestColSum);
+    Console.WriteLine($"Part 1: {lowestSum}");
+}
