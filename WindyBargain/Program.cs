5968d50ad2812292c1979899116723ee63d2059c
Magnus Olofsson <magnusolofsson@outlook.com>
2025-04-11 08:11:14 +0200

    Solved WindyBargain p3

--- WindyBargain/Program.cs
+++ WindyBargain/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
+using System.Reflection;
 using System.Text.RegularExpressions;
+using System.Transactions;
 
 const string fileName = "input.txt";
 
@@ -11,11 +13,8 @@ Part2(officials,transactions);
 
 (officials, transactions) = ParseInput(fileName);
 Part3(officials, transactions);
+
 void Part3(List<Official> officials, List<Transaction> transactions)
-{
-    
-}
-void Part2(List<Official> officials, List<Transaction> transactions)
 {
     int transactionCount = 0;
     foreach (var transaction in transactions)
@@ -24,18 +23,26 @@ void Part2(List<Official> officials, List<Transaction> transactions)
         Official? recipient = officials.Where(o => o.Name == transaction.To).FirstOrDefault();
         if (sender != null && recipient != null)
         {
-            int amount;
-            if (transaction.Amount > sender.Balance)
-            {
-                amount = sender.Balance;
-            }
-            else
-            {
-                amount = transaction.Amount;
-            }
-            sender.Balance -= amount;
-            recipient.Balance += amount;
-            transactionCount++;
+           sender.SendPaymentPart3(recipient, transaction.Amount);
+        }
+        else
+        {
+            Console.WriteLine("Error: sender or recipient could not be found.");
+        }
+    }
+    var topThree = officials.OrderByDescending(o => o.Balance).Take(3).ToArray();
+    int sumPart3 = topThree.Sum(o => o.Balance);
+    Console.WriteLine($"Part 3: {sumPart3}");
+}
+void Part2(List<Official> officials, List<Transaction> transactions)
+{
+    foreach (var transaction in transactions)
+    {
+        Official? sender = officials.Where(o => o.Name == transaction.From).FirstOrDefault();
+        Official? recipient = officials.Where(o => o.Name == transaction.To).FirstOrDefault();
+        if (sender != null && recipient != null)
+        {
+            sender.SendPaymentPart2(recipient, transaction.Amount);
         }
         else
         {
@@ -56,8 +63,7 @@ void Part1(List<Official> officials, List<Transaction> transactions)
         Official recipient = officials.Where(o => o.Name == transaction.To).FirstOrDefault();
         if (sender != null && recipient != null)
         {
-            sender.Balance -= transaction.Amount;
-            recipient.Balance += transaction.Amount;
+            sender.SendPaymentPart1(recipient,transaction.Amount);
         }
     }
 
@@ -102,4 +108,64 @@ class Official
         return $"{Name} - {Balance}";
     }
 
+    public List<Debt> Debts { get; set; } = new ();
+
+    public void PayDebts()
+    {
+        while(this.Balance > 0 && this.Debts.Count > 0)
+        {
+            Debt debt = this.Debts[0];
+            var amountToPay = Math.Min(this.Balance, debt.Amount);
+            debt.Amount -= amountToPay;
+
+            if(debt.Amount == 0)
+            {
+                this.Debts.RemoveAt(0);
+            }
+            this.SendPaymentPart3(debt.To, amountToPay);
+        }
+    }
+
+    public void SendPaymentPart3(Official to, int amount)
+    {
+        var amountToSend = Math.Min(this.Balance, amount);
+        if(amount > this.Balance)
+        {
+            this.Debts.Add(new Debt(to, amount - this.Balance));
+        }
+        this.Balance -= amountToSend;
+        to.Balance += amountToSend;
+        to.PayDebts(); // anyone who has received funds should try to pay debts.
+    }
+
+    public void SendPaymentPart2(Official to, int amount)
+    {
+        
+        var amountToSend = Math.Min(this.Balance, amount);
+        
+        this.Balance -= amountToSend;
+        to.Balance += amountToSend;
+    }
+
+    public void SendPaymentPart1(Official to, int amount)
+    {
+        this.Balance -= amount;
+        to.Balance += amount;
+    }
+
+}
+
+class Debt
+{
+    public Official To { get; set; }
+    public int Amount { get; set; }
+    public Debt(Official to, int amount)
+    {
+        To = to;
+        Amount = amount;
+    }
+    public override string ToString()
+    {
+        return $"Debt of amount: {Amount} to Official: {To.Name}";
+    }
 }
